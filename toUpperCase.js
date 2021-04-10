09b6e38f2a6dc9b4e77ed1bf056c638347144a5c
Ghazanfar-ijaz <ghazanfar@outlook.fr>
2021-04-10 21:02:45 +0200

    ok

---
+++ toUpperCase.js
@@ -0,0 +1,29 @@
+/*
+Convertir le code suivant qui utilise les versions synchrones de l'api du module fs,
+en un code qui utilise les versions asynchrones de l'api du module fs/promises.
+
+const fs = require('fs')
+try {
+  let txt1 = fs.readFileSync('hello.txt', 'utf-8')
+  txt1 = txt1.toUpperCase()
+  fs.writeFileSync('hello.txt', txt1)
+} catch (e) {
+  console.log(e.message)
+}
+
+*/
+
+const fsPromises = require('fs/promises')
+
+const main = async () => {
+  try {
+    let txt1 = await fsPromises.readFileSync('hello.txt', 'utf-8')
+    txt1 = txt1.toUpperCase()
+    fsPromises.writeFileSync('hello.txt', txt1)
+  } catch (e) {
+    console.log(e.message)
+  }
+}
+
+main()
+
